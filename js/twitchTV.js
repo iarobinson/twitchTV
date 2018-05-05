8ce6d089e36257dd09b4e17ad9a232b0679cecbc
Ian <ian (at) ianrobinson.net>
2018-05-05 17:50:50 +0200

    Remove all bootstrap and jQuery code. Move model data to a separate JS file. Simplify CSS. Convert index.html to HTML5 specifications

--- js/twitchTV.js
+++ js/twitchTV.js
@@ -1,152 +1,28 @@
-var twitchStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+console.log("twitchTV.js loaded");
+window.onload = init;
 
-var dataSimulation = [
-  {
-    "stream": {
-      "mature": false,
-      "status": "Greg working on Electron-Vue boilerplate w/ Akira #programming #vuejs #electron",
-      "broadcaster_language": "en",
-      "display_name": "FreeCodeCamp",
-      "game": "Creative",
-      "language": "en",
-      "_id": 79776140,
-      "name": "freecodecamp",
-      "created_at": "2015-01-14T03:36:47Z",
-      "updated_at": "2016-09-17T05:00:52Z",
-      "delay": null,
-      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png",
-      "banner": null,
-      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png",
-      "background": null,
-      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_banner-6f5e3445ff474aec-480.png",
-      "profile_banner_background_color": null,
-      "partner": false,
-      "url": "https://www.twitch.tv/freecodecamp",
-      "views": 161989,
-      "followers": 10048,
-      "_links": {
-        "self": "https://api.twitch.tv/kraken/channels/freecodecamp",
-        "follows": "https://api.twitch.tv/kraken/channels/freecodecamp/follows",
-        "commercial": "https://api.twitch.tv/kraken/channels/freecodecamp/commercial",
-        "stream_key": "https://api.twitch.tv/kraken/channels/freecodecamp/stream_key",
-        "chat": "https://api.twitch.tv/kraken/chat/freecodecamp",
-        "subscriptions": "https://api.twitch.tv/kraken/channels/freecodecamp/subscriptions",
-        "editors": "https://api.twitch.tv/kraken/channels/freecodecamp/editors",
-        "teams": "https://api.twitch.tv/kraken/channels/freecodecamp/teams",
-        "videos": "https://api.twitch.tv/kraken/channels/freecodecamp/videos"
-      }
-    },
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/freecodecamp",
-      "channel": "https://api.twitch.tv/kraken/channels/freecodecamp"
-    }
-  },
-  {
-    "stream": null,
-    "display_name": "OgamingSC2",
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/ogamingsc2",
-      "channel": "https://api.twitch.tv/kraken/channels/ogamingsc2"
-    }
-  },
-  {
-    "stream": {
-      "mature": false,
-      "status": "RERUN: StarCraft 2 - Kane vs. HuK (ZvP) - WCS Season 3 Challenger AM - Match 4",
-      "broadcaster_language": "en",
-      "display_name": "ESL_SC2",
-      "game": "StarCraft II",
-      "language": "en",
-      "_id": 30220059,
-      "name": "esl_sc2",
-      "created_at": "2012-05-02T09:59:20Z",
-      "updated_at": "2016-09-17T06:02:57Z",
-      "delay": null,
-      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_image-d6db9488cec97125-300x300.jpeg",
-      "banner": null,
-      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-channel_offline_image-5a8657f8393c9d85-1920x1080.jpeg",
-      "background": null,
-      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_banner-f8295b33d1846e75-480.jpeg",
-      "profile_banner_background_color": "#050506",
-      "partner": true,
-      "url": "https://www.twitch.tv/esl_sc2",
-      "views": 60843789,
-      "followers": 135275,
-      "_links": {
-        "self": "https://api.twitch.tv/kraken/channels/esl_sc2",
-        "follows": "https://api.twitch.tv/kraken/channels/esl_sc2/follows",
-        "commercial": "https://api.twitch.tv/kraken/channels/esl_sc2/commercial",
-        "stream_key": "https://api.twitch.tv/kraken/channels/esl_sc2/stream_key",
-        "chat": "https://api.twitch.tv/kraken/chat/esl_sc2",
-        "subscriptions": "https://api.twitch.tv/kraken/channels/esl_sc2/subscriptions",
-        "editors": "https://api.twitch.tv/kraken/channels/esl_sc2/editors",
-        "teams": "https://api.twitch.tv/kraken/channels/esl_sc2/teams",
-        "videos": "https://api.twitch.tv/kraken/channels/esl_sc2/videos"
-      }
-    },
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
-      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
-    }
-  },
-  {
-    "stream": null,
-    "display_name": "noobs2ninjas",
-    "_links": {
-      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
-      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
-    }
-  },
-  {
-    "error": "Not Found",
-    "status": 404,
-    "message": "Channel 'not-a-valid-account' does not exist"
-  }
-];
-
-// Begin jQuery
-
-$(document).ready(function() {
-  //Set cor
+function init() {
   var url = 'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?'
-  
-  // Get info from API call
-  $.getJSON(url, function(data) {
-    if (data.stream === null) {
-      $('#fccStatus').html("FCC is offline.");
-    } else {
-      $('#fccStatus').html("FCC is online.");
-    }
-    console.log(data);
-  });
 
-  
-  // Functionallity of buttons being clicked
-  function buttonClicked(button) {
-    $(this).children().remove();
-    for (var i = 0; i < dataSimulation.length; i += 1) {
-      var liveStreams = dataSimulation[i].display_name;
-      
-      if (this.liveStreams) {
-        $('.twitchInfo').append("<li>" + liveStreams + "</li>");
-      }
-    }
-  }
-  
-  
-  // Below three buttons
-  $('.live').on('click', function() {
-    console.log('live button clicked');
-    buttonClicked('display_name');
-  });
-  
-  $('.all').on('click', function() {
-    console.log('all button clicked');
-    buttonClicked('display_name');
-  });
-  
-  $('.offline').on('click', function() {
-    console.log('offline button clicked');
-    buttonClicked('display_name');
-  });
-});
+  // // MUST REVISIT HERE
+  // $.getJSON(url, function(data) {
+  //   if (data.stream === null) {
+  //     $('#fccStatus').html("FCC is offline.");
+  //   } else {
+  //     $('#fccStatus').html("FCC is online.");
+  //   }
+  //   console.log(data);
+  // });
+
+  // // Rebuild with JS - Simple Button Interaction
+  // function buttonClicked(button) {
+  //   $(this).children().remove();
+  //   for (var i = 0; i < dataSimulation.length; i += 1) {
+  //     var liveStreams = dataSimulation[i].display_name;
+  //
+  //     if (this.liveStreams) {
+  //       $('.twitchInfo').append("<li>" + liveStreams + "</li>");
+  //     }
+  //   }
+  // }
+}
