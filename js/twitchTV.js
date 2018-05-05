4d2b7d9e4ebea916b2407eb686a65e5749604b66
Ian <ian (at) ianrobinson.net>
2018-05-05 17:39:24 +0200

    Revisiting this codebase after a long time - Planning on gutting it of Bootstrap and jQuery and making it solely html5

--- js/twitchTV.js
+++ js/twitchTV.js
@@ -104,23 +104,37 @@ var dataSimulation = [
   }
 ];
 
+// Begin jQuery
+
 $(document).ready(function() {
+  //Set cor
+  var url = 'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?'
   
-  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+  // Get info from API call
+  $.getJSON(url, function(data) {
+    if (data.stream === null) {
+      $('#fccStatus').html("FCC is offline.");
+    } else {
+      $('#fccStatus').html("FCC is online.");
+    }
     console.log(data);
   });
 
+  
+  // Functionallity of buttons being clicked
   function buttonClicked(button) {
     $(this).children().remove();
     for (var i = 0; i < dataSimulation.length; i += 1) {
       var liveStreams = dataSimulation[i].display_name;
       
-      if (liveStreams) {
+      if (this.liveStreams) {
         $('.twitchInfo').append("<li>" + liveStreams + "</li>");
       }
     }
   }
   
+  
+  // Below three buttons
   $('.live').on('click', function() {
     console.log('live button clicked');
     buttonClicked('display_name');
